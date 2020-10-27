ee6d31716d1777a42ba242a047b3dc2bf1a8fb10
Bharat Poptwani <bharat.poptwani@gmail.com>
2020-10-27 22:51:58 +0530

    added AbortController

--- ui-components/live-search-ts/src/components/LiveSearch/index.tsx
+++ ui-components/live-search-ts/src/components/LiveSearch/index.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+
+import SearchListing from './SearchListing';
 
 function LiveSearch() {
-  return <div>LiveSearch</div>;
+  const [query, setQuery] = useState('');
+  const [jokes, setJokes] = useState([]);
+  const focusSearch = useRef(null);
+
+  useEffect(() => {
+    focusSearch.current.focus();
+  }, []);
+
+  const getJokes = async (query: string, controller?: AbortController) => {
+    const res = await axios(`https://icanhazdadjoke.com/search?term=${query}`);
+    const jokesData = await res.data.results;
+    return jokesData;
+  };
+
+  const sleep = (ms: number) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  useEffect(() => {
+    let currentQuery = true;
+    const abortController = new AbortController();
+
+    const loadJokes = async () => {
+      if (!query) return setJokes([]);
+
+      await sleep(350);
+      if (currentQuery) {
+        const jokes = await getJokes(query, abortController);
+        setJokes(jokes);
+      }
+    };
+    loadJokes();
+
+    return () => {
+      currentQuery = false;
+      abortController.abort();
+    };
+  }, [query]);
 }
 
 export default LiveSearch;
